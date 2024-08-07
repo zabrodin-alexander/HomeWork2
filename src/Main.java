3421c82e2a09866060a16ed1b3835968fc0f50b1
Zabro <zabrodin.alexander51@gmail.com>
2024-08-07 11:05:32 +0300

    Исправлены ошибки

--- src/Main.java
+++ src/Main.java
@@ -22,12 +22,14 @@ public class Main {
         System.out.println(paper - 7639);
 
         var friend = 19;
+        System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend= friend/7;
         System.out.println(friend);
 
         var frog = 3.5;
+        System.out.println(frog);
         frog = frog*10;
         System.out.println(frog);
         frog = frog/3.5;
