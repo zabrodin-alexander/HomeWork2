f36dc04762dd858bab3060b05b922736571a619a
Zabro <zabrodin.alexander51@gmail.com>
2024-08-24 13:51:23 +0300

    ch

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println();
 
         dog = dog + 4;
         cat = cat + 4;
@@ -13,6 +14,7 @@ public class Main {
         System.out.println(dog + 4);
         System.out.println(cat + 4);
         System.out.println(paper + 4);
+        System.out.println();
 
         dog = dog - 3.5;
         cat = cat - 1.6;
@@ -20,6 +22,7 @@ public class Main {
         System.out.println(dog - 3.5);
         System.out.println(cat - 1.6);
         System.out.println(paper - 7639);
+        System.out.println();
 
         var friend = 19;
         System.out.println(friend);
@@ -27,6 +30,7 @@ public class Main {
         System.out.println(friend);
         friend= friend/7;
         System.out.println(friend);
+        System.out.println();
 
         var frog = 3.5;
         System.out.println(frog);
@@ -36,23 +40,22 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+        System.out.println();
 
         var oneBoxer = 78.2;
         var twoBoxer = 82.7;
         var totalWeight = oneBoxer + twoBoxer;
         System.out.println(totalWeight);
-
         var difference = oneBoxer - twoBoxer;
         System.out.println("Разица между массами бойцов " + difference + "кг!");
-
         var remainder = twoBoxer % oneBoxer;
         System.out.println("Остаток от деления между двумя весами " + remainder + "кг!");
+        System.out.println();
 
         var hours = 640;
         var oneEmployee = 8;
         var totalEmployees = hours/oneEmployee;
         System.out.println("Всего работников в компании - " + totalEmployees + " человек ");
-
         totalEmployees = totalEmployees + 94;
         var totalHours = totalEmployees * oneEmployee;
         System.out.println("Если в компании работает " + totalEmployees + " человека, то всего " + totalHours
